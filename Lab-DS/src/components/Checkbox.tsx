a08d71237d7a9310f676aed2a514aff2c63cba9d
Fernando Pinto <fschultepinto@gmail.com>
2022-10-13 20:25:46 -0300

    Finalização com decorator

--- Lab-DS/src/components/Checkbox.tsx
+++ Lab-DS/src/components/Checkbox.tsx
@@ -9,7 +9,7 @@ export function Checkbox({}: CheckboxProps) {
     <CheckboxPrimitive.Root className="w-6 h-6 bg-gray-800 p-[2px] rounded">
 
     <CheckboxPrimitive.Indicator asChild>
-      <Check weight="bold" className=" h-5 w-5 text-cyan-500" />
+      <Check weight="bold" className="h-5 w-5 text-cyan-500" />
     </CheckboxPrimitive.Indicator>
     </CheckboxPrimitive.Root>
 
